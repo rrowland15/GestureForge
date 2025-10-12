f897b08e0758f4fd33c809ce7be03ac89b13e457
Ryan Rowland <rcrowland15@gmail.com>
2025-10-11 21:57:30 -0700

    Set up initial app structure, verified tailwind working properly, added project plan/outline to Readme

---
+++ gestureforge/src/app/layout.tsx
@@ -0,0 +1,20 @@
+
+import "./globals.css";
+
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`$antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
